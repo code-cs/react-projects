023f31e4e8258cc528ef1953532502a57fa4be66
Monireh Rasouli <monireh.rasouli20@gmail.com>
2021-06-07 14:05:40 +0100

    Done Birthday Reminder project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+// {
+//   id: 1,
+//   name: 'Bertie Yates',
+//   age: 29,
+//   image:
+//     'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+// },
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+ 
+  return <main>
+    <section className='container'>
+      <h3> {people.length} birthday today!</h3>
+      <List people={people} />
+      <button onClick={() => setPeople([])}> Clear All</button>
+    </section>
+  </main>;
 }
 
 export default App;
