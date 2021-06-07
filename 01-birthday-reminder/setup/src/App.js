72abe6cd780d5b93623c46b53d36767c389ab527
Monireh Rasouli <monireh.rasouli20@gmail.com>
2021-06-07 14:08:16 +0100

    Removed unnecessary comments

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,13 +2,6 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 
-// {
-//   id: 1,
-//   name: 'Bertie Yates',
-//   age: 29,
-//   image:
-//     'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-// },
 function App() {
   const [people, setPeople] = useState(data);
  
